2fc19222af67f9070a54ff8e203d206bd94e2917
Mohamed Lemroudi <mohamed.lemroudi@mapex.io>
2025-09-21 15:15:48 +0200

    Fill the tables.

---
+++ dbfinancial_test.sql
@@ -0,0 +1,17 @@
+-- TEST
+
+INSERT INTO Users VALUES('Mohamed','123');
+
+SELECT * FROM Users;
+
+INSERT INTO Bank_accounts VALUES (1,'Revolut', 'Online', 3000);
+
+SELECT * FROM Bank_accounts;
+
+INSERT INTO Categories VALUES (1, 'Food', 3, 1, 'PERSONAL');
+
+SELECT * FROM Categories;
+
+INSERT INTO Transactions VALUES (1, 100, '20250921');
+
+SELECT * FROM Transactions;
